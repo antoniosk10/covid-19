73a695ad7bafa9be23a02436a718e7fd1fb301d9
Anton_Skorobogaty <a.skorobogatiy@sotbit.ru>
2020-12-22 20:07:23 +0300

    feat:set global cases form api

--- src/GlobalCases.js
+++ src/GlobalCases.js
@@ -1,10 +1,18 @@
+import Utils from './Utils';
+
+const utils = new Utils();
+
 class GlobalCases {
   constructor() {
     this.innerContainer = document.querySelector('.global-cases .section__inner');
     this.btnExpand = this.innerContainer.querySelector('[data-btn-expand]');
+    this.globalQuantity = document.querySelector('.global-cases__quantity');
   }
 
   init() {
+    utils.infoForAllPeriod().then((data) => {
+      this.setGlobalCases(data);
+    }).catch(() => { this.createMessageError(); });
     this.setEventBtnExpand();
   }
 
@@ -12,6 +20,14 @@ class GlobalCases {
     this.btnExpand.addEventListener('click', () => {
       this.innerContainer.classList.toggle('expanded');
     });
+  }
+
+  setGlobalCases(data) {
+    this.globalQuantity.textContent = data.Global.TotalConfirmed;
+  }
+
+  createMessageError() {
+    this.globalQuantity.textContent = 'Try Later :(';
   }
 }
 
