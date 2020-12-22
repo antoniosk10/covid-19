46957ec9d2eb40f5061945dcc73cfbce4bdb0548
Anton_Skorobogaty <a.skorobogatiy@sotbit.ru>
2020-12-22 19:45:18 +0300

    feat:set country list from api

--- src/CountriesList.js
+++ src/CountriesList.js
@@ -1,12 +1,20 @@
+import Utils from './Utils';
+
+const utils = new Utils();
+
 class CountriesList {
   constructor() {
     this.arrayCountries = Array.prototype.slice.call(document.querySelectorAll('.countries-cases__list-item'));
     this.search = document.querySelector('[data-search-country]');
     this.innerContainer = document.querySelector('.countries-cases .section__inner');
     this.btnExpand = this.innerContainer.querySelector('[data-btn-expand]');
+    this.countryList = document.querySelector('.countries-cases__list');
   }
 
   init() {
+    utils.infoForAllPeriod().then((data) => {
+      this.setCountryList(data);
+    }).catch(() => { CountriesList.createMessageError(); });
     this.setEventSearchCountry();
     this.setEventBtnExpand();
   }
@@ -24,6 +32,7 @@ class CountriesList {
   }
 
   filterCountry(value) {
+    this.arrayCountries = Array.prototype.slice.call(document.querySelectorAll('.countries-cases__list-item'));
     this.showAllCountry();
     this.arrayCountries.forEach((el) => {
       const countryItem = el;
@@ -37,8 +46,27 @@ class CountriesList {
   showAllCountry() {
     this.arrayCountries.forEach((el) => {
       const countryItem = el;
-      countryItem.style.display = 'block';
+      countryItem.style.display = 'flex';
     });
+  }
+
+  setCountryList(data) {
+    this.countryList.innerHTML = CountriesList.createCountryList(data);
+  }
+
+  static createCountryList(data) {
+    let layout = '';
+    data.Countries.forEach((el) => {
+      layout += `<li class="countries-cases__list-item">
+      <span class="countries-cases__quantity">${el.TotalConfirmed}</span>
+      <span class="countries-cases__country-name">${el.Country}</span>
+      </li>`;
+    });
+    return layout;
+  }
+
+  static createMessageError() {
+    this.countryList.innerHTML = 'Try Later :(';
   }
 }
 
